fbfb9d1000a12a515f3f4914c40ec5fc44de11c6
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:16:38 +0800

    Product listings page done

---
+++ productListings.php
@@ -0,0 +1,15 @@
+<?php 
+// header
+include("./Pages/Shared/header.php"); 
+?>
+
+<?php
+// render categories
+include("./Pages/ProductCatalogue/Listings/title.php");
+include("./Pages/ProductCatalogue/Listings/listings.php");
+?>
+
+<?php 
+// footer
+include("./Pages/Shared/footer.php"); 
+?>
