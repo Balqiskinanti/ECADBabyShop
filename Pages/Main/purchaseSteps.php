50a1afb585b94b463ecd594303980cbf0f7a7270
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:13:41 +0800

    Add purchase steps component

---
+++ Pages/Main/purchaseSteps.php
@@ -0,0 +1,27 @@
+<div id="purchase-steps">
+    <div id="promotion-list-title">
+        HOW TO PURCHASE?
+    </div>
+
+    <div class="container-fluid m-0" id="purchase-steps-container">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-2">
+                <i class="fa fa-user-circle"></i>
+                <p class="steps-title">Login</p>
+                <p class="steps-desc">Create an account with us. and you can modify the details later!</p>
+            </div>
+            <div class="col-sm-2">
+                <i class="fa fa-cart-plus"></i>
+                <p class="steps-title">Checkout</p>
+                <p class="steps-desc">Select items you want to buy, add to cart and checkout using PayPal.</p>
+            </div>
+            <div class="col-sm-2">
+                <i class="fa fa-truck"></i>
+                <p class="steps-title">Deliver</p>
+                <p class="steps-desc">You can opt for express delivery  for delivery within 24 hours. </p>
+            </div>
+            <div class="col-3"></div>
+        </div>
+    </div>
+</div>
