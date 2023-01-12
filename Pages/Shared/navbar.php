5e931b2c5c2e82afa3d820e354aaa83d3574807b
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:11:32 +0800

    Init basic navbar

---
+++ Pages/Shared/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-md bg-light navbar-light sticky-top">
+    <!-- Logo -->
+    <a class="navbar-brand" href="#">
+        Logo
+    </a>
+    
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse justify-content-end links" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Product Catalogue</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Search Product</a>
+        </li>
+        </ul>
+    </div>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a type="button" class="nav-link btn btn-outline-secondary" href="#">Button1</a>
+            </li>
+            <li class="nav-item">
+            <a  type="button" class="nav-link btn btn-dark" href="#">Button2</a>
+            </li>
+        </ul>
+    </div>
+</nav>
