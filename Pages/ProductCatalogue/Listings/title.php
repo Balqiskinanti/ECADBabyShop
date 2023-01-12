74be39f93103fae5f213d5930a59ff6ad594303c
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:16:08 +0800

    Add title component (LISTINGS)

---
+++ Pages/ProductCatalogue/Listings/title.php
@@ -0,0 +1,7 @@
+<div id="brand-name">
+    EBS
+</div>
+
+<div id="promotion-list-title" style="padding-right:50px;">
+    PRODUCT LISTINGS FOR <?php echo "<span style='text-transform: uppercase'>" . $_GET["catName"] . "</span>" ?>
+</div>
