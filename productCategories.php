bf04a274d55697c9f32a3ae498fa8b1c989b01e4
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:15:51 +0800

    Categories page done

---
+++ productCategories.php
@@ -0,0 +1,15 @@
+<?php 
+// header
+include("./Pages/Shared/header.php"); 
+?>
+
+<?php
+// render categories
+include("./Pages/ProductCatalogue/Categories/title.php");
+include("./Pages/ProductCatalogue/Categories/categories.php");
+?>
+
+<?php 
+// footer
+include("./Pages/Shared/footer.php"); 
+?>
