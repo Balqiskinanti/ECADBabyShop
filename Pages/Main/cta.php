ff31e54e7220ba7d362b21544b9efd7f76db4a94
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:12:34 +0800

    Add call to action component (HOME)

---
+++ Pages/Main/cta.php
@@ -0,0 +1,26 @@
+<div id="landing">
+    <div id="brand-name">
+        EBS
+    </div>
+
+    <div id="cta">
+        <div class="container-fluid m-0">
+            <div class="row">
+                <div class="col-sm-6" id="cta-text">
+                    <h1>
+                        Treat Your <span style="color: var(--logo-color);">Little Ones</span> <br/>
+                        With the Best Gifts
+                    </h1>
+                    <p>
+                        EBS delivers high quality, age-appropriate, and <br/>
+                        comfortable new-born items straight to your door.
+                    </p>
+                    <a  type="button" class="btn btn-dark" href="#" id="cta-btn">Get Started</a>
+                </div>
+                <div class="col-sm-6" id="cta-img">
+                    <img src="./Images/Template/Baby.png" alt="Baby image">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
