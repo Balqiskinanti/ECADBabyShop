97a8000a3855487c79bc327ac93ebf5cf0dbb8c0
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:12:04 +0800

    Init footer

---
+++ Pages/Shared/footer.php
@@ -0,0 +1,21 @@
+<div class="back-to-top" style="font-size: small; padding-top: 10px; text-align: center;">
+    <a href="#" style="color: black;"><b>Back to Top <i class="fas fa-angle-up"></i></b></a>
+</div>
+
+<hr/>
+
+<div id="footer" style="padding: 30px;justify-content: space-evenly; font-size: small;">
+    <div class="container-fluid m-0">
+        <div class="row">
+            <div class="col" style="text-align: left;">
+                Logo
+            </div>
+            <div class="col" style="text-align: center;">
+                &#169 Copyright
+            </div>
+            <div class="col" style="text-align: end;">
+                <a href="#" style="color: black;">Link 1</a> 
+            </div>
+        </div>
+    </div>
+</div>
