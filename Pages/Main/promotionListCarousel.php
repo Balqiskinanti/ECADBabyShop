c8912601a43a9868e27767a4753c2881e34a3c90
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-13 00:13:02 +0800

    Add promotion list component (HOME)

---
+++ Pages/Main/promotionListCarousel.php
@@ -0,0 +1,147 @@
+<div id="promotion-list-carausel">
+    <div id="promotion-list-small-title">
+        PROMOTION
+    </div>
+
+    <div id="promotion-list-title">
+        GRAB THEM BEFORE THEY'RE GONE 🛒
+    </div>
+
+    <section class="pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                </div>
+                <div class="col-6 text-right">
+                    <a class="btn btn-primary mb-3 mr-1 btn-carausel" href="#carouselExampleIndicators2" role="button" data-slide="prev" style="background-color: white;border:none;color:black; padding:2px!important">
+                        <i class="fa fa-arrow-left"></i>
+                    </a>
+                    <a class="btn btn-primary mb-3 btn-carausel" href="#carouselExampleIndicators2" role="button" data-slide="next" style="background-color: white;border:none;color:black;padding:2px!important">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-12">
+                    <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+    
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/California_Baby_Shampoo_And_Bodywash-Calming.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00 <span class="price-before">$10.00</span></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Chicco_Band_Baby_Walker.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Cocotina_Rose_Flower_Petti_Ruffle_Romper.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Fisher-Price_Portable_Rocker.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Joovy_Caboose_Ultralight_Stand-On_Tandem_Stroller.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Konicare_Minyak_Telon.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Panda_Romper.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="img-wrapper">
+                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Puku_Mini_Bath_Tub.jpg">
+                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
+                                            </div>
+                                            <div class="card-body">
+                                                <h4 class="card-title"><b>Product Name</b></h4>
+                                                <p class="card-text">$20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
