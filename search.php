d816eec3e46f177cc52cbdf576e48a1fb31777c2
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-15 18:32:33 +0800

    Finish search page

---
+++ search.php
@@ -0,0 +1,15 @@
+<?php 
+// header
+include("./Pages/Shared/header.php"); 
+?>
+
+<?php
+// render search
+include("./Pages/Search/title.php");
+include("./Pages/Search/search.php");
+?>
+
+<?php 
+// footer
+include("./Pages/Shared/footer.php"); 
+?>
