810b0ebed98b197bc663ae60edce5f72fad40ec3
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-15 18:33:29 +0800

    Swithc hardcoded products to items in db

--- Pages/Main/promotionListCarousel.php
+++ Pages/Main/promotionListCarousel.php
@@ -1,3 +1,90 @@
+<?php
+// current session & db conn
+include_once("mySQLConn.php");
+
+$qry = "SELECT *
+From catproduct cp 
+INNER JOIN product p 
+ON cp.ProductID=p.ProductID
+INNER JOIN category c
+ON c.CategoryID = cp.CategoryID
+WHERE p.Offered = 1 AND
+p.OfferStartDate <= now() AND
+p.OfferEndDate >= now() AND
+p.Quantity > 0
+";
+
+$result = $conn->query($qry);
+
+// close connection
+$conn->close();
+
+// categories containers 
+$htmlElement = "";
+$htmlElementSecondPage = "";
+$count = 1;
+while($row = $result->fetch_array()){
+    $imgPath = $row["ProductImage"];
+    $title = $row["ProductTitle"];
+    $cid = $row["CategoryID"];
+    $catName= $row["CatName"];
+
+    $now = new DateTime('now');
+    ($now->format('Y-m-d') >= $row["OfferStartDate"]  && $now->format('Y-m-d') <= $row["OfferEndDate"]) ? $isOfferStillOnGoing = true : $isOfferStillOnGoing = false;
+    ($row["Offered"] == 1 && $isOfferStillOnGoing) ? $price =  "$" . $row["OfferedPrice"] : $price = "$" . $row["Price"];
+    ($row["Offered"] == 1 && $isOfferStillOnGoing) ? $oldPrice =  "$" . $row["Price"] : $oldPrice = "";
+
+    // indicators : offer & out of stock
+    // if still offerred but out of stock, out of stock indicator will be shown
+    $indicatorHTML = "";
+    ($row["Offered"] == 1 && $isOfferStillOnGoing) ? $indicatorHTML = "<div style='text-align: center; background-color: #0ACF83; color: white;'>On Offer</div>" : $indicatorHTML = "";
+    ($row["Quantity"] == 0) ? $indicatorHTML = "<div style='text-align: center; background-color: red; color: white;'>Out of Stock</div>" : $indicatorHTML = $indicatorHTML;
+
+    if($count <= 3){
+        $count+=1;
+        $htmlElement .= '
+            <div class="col-md-4 mb-3">
+                <a href="productDetails.php?pid=' . $row["ProductID"] . '&' . 'productName=' . $title . '&' . 'cid=' . $cid . '&' . 'catName=' . $catName . '" style="text-decoration:none;color:black;">
+                    <div class="card h-100">
+                        <div class="img-wrapper">
+                            <img class="img-fluid" alt="100%x280" src="./Images/Products/' . $imgPath .'">' . 
+                            
+                            $indicatorHTML
+
+                        . '</div>
+                        <div class="card-body">
+                            <h4 class="card-title"><b>' . $title . '</b></h4>
+                            <p class="card-text" style="padding-top:10px;">' . $price . '<span class="price-before">' . $oldPrice . '</span></p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        ';
+    }
+    elseif($count <=6){
+        $count+=1;
+        $htmlElementSecondPage .= '
+            <div class="col-md-4 mb-3">
+                <a href="productDetails.php?pid=' . $row["ProductID"] . '&' . 'productName=' . $title . '&' . 'cid=' . $cid . '&' . 'catName=' . $catName . '" style="text-decoration:none;color:black;">
+                    <div class="card h-100">
+                        <div class="img-wrapper">
+                            <img class="img-fluid" alt="100%x280" src="./Images/Products/' . $imgPath .'">' . 
+                            
+                            $indicatorHTML
+
+                        . '</div>
+                        <div class="card-body">
+                            <h4 class="card-title"><b>' . $title . '</b></h4>
+                            <p class="card-text" style="padding-top:10px;">' . $price . '<span class="price-before">' . $oldPrice . '</span></p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        ';
+    }
+}
+?>
+
 <div id="promotion-list-carausel">
     <div id="promotion-list-small-title">
         PROMOTION
@@ -27,117 +114,23 @@
                             <div class="carousel-item active">
                                 <div class="row">
 
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/California_Baby_Shampoo_And_Bodywash-Calming.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00 <span class="price-before">$10.00</span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Chicco_Band_Baby_Walker.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Cocotina_Rose_Flower_Petti_Ruffle_Romper.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <?php echo $htmlElement ?>
     
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <div class="row">
-    
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Fisher-Price_Portable_Rocker.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Joovy_Caboose_Ultralight_Stand-On_Tandem_Stroller.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Konicare_Minyak_Telon.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-    
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="row">
-    
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Panda_Romper.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <div class="img-wrapper">
-                                                <img class="img-fluid" alt="100%x280" src="./Images/Products/Puku_Mini_Bath_Tub.jpg">
-                                                <div style="text-align: center; background-color: #0ACF83; color: white;">On Offer</div>
-                                            </div>
-                                            <div class="card-body">
-                                                <h4 class="card-title"><b>Product Name</b></h4>
-                                                <p class="card-text">$20.00</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <?php 
+                                if($htmlElementSecondPage != ""){
+                                    echo '
+                                        <div class="carousel-item">
+                                            <div class="row">' .
+                
+                                                $htmlElementSecondPage
 
-                                </div>
-                            </div>
+                                            .'</div>
+                                        </div>
+                                    ';
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
