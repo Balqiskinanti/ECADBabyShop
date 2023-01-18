81a30ec5815c27e5642e55d29a013e90cd83c283
JiaYongg <s10202579@connect.np.edu.sg>
2023-01-18 20:36:04 +0800

    Fixed login error

--- Pages/Shared/navbar.php
+++ Pages/Shared/navbar.php
@@ -1,3 +1,50 @@
+<?php
+
+    // <!-- Navbar links -->
+    $content1 = "<div class='collapse navbar-collapse justify-content-end' id='collapsibleNavbar'>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+            <a type='button' class='nav-link btn btn-outline-secondary' href='../ECADBabyShop/login.php'>Log In</a>
+            </li>
+            <li class='nav-item'>
+            <a  type='button' class='nav-link btn btn-dark' href='../ECADBabyShop/register.php'>Register</a>
+            </li>
+        </ul>
+    </div>
+    ";
+
+
+    if(isset($_SESSION["ShopperName"])) { 
+        //Display a greeting message, Change Password and logout links 
+        //after shopper has logged in.
+
+        $content1 = "
+        <div class='collapse navbar-collapse justify-content-end' id='collapsibleNavbar'>
+            <ul class='navbar-nav'>
+                <li class='nav-item'>
+                    <a type='button' class='nav-link btn btn-outline-secondary' href='#'>cartNum / totalAmt <img src='../ECADBabyShop/Images/Login/shopping-bag.png' width='30' height='30'></a>
+                </li>
+                <li class='nav-item'>
+                    <a type='button' class='nav-link btn' href='#'>$_SESSION[ShopperName] <img src='../ECADBabyShop/Images/Login/user.png' width='30' height='30'></a>
+                    <ul class='navbar-nav'>
+                        <li class='nav-item'><a class='nav-link' href='#'>Change Password</a></li>
+                        <li class='nav-item'><a class='nav-link' href='#'>Update Profile</a></li>
+                        <li class='nav-item'><a class='nav-link' href='#'>Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        ";
+
+        // //Display number of item in cart
+        // if (isset($_SESSION["NumCartItem"]))
+        // {
+        //     if ($_SESSION["NumCartItem"] != 0)
+        //         $content1 .= ", $_SESSION[NumCartItem] item(s) in shopping cart";
+        // }
+    }
+?>
+
 <nav class="navbar navbar-expand-md bg-light navbar-light sticky-top">
     <!-- Logo -->
     <a class="navbar-brand" href="#">
@@ -21,65 +68,5 @@
         </ul>
     </div>
 
-    <!-- rmeove later -->
-   <div class='collapse navbar-collapse justify-content-end' id='collapsibleNavbar'>
-        <ul class='navbar-nav'>
-            <li class='nav-item'>
-            <a type='button' class='nav-link btn btn-outline-secondary' href='#'>Log In</a>
-            </li>
-            <li class='nav-item'>
-            <a  type='button' class='nav-link btn btn-dark' href='#'>Register</a>
-            </li>
-        </ul>
-    </div>
-
-
-
-    <?php
-
-    // <!-- Navbar links -->
-    $content1 = "<div class='collapse navbar-collapse justify-content-end' id='collapsibleNavbar'>
-        <ul class='navbar-nav'>
-            <li class='nav-item'>
-            <a type='button' class='nav-link btn btn-outline-secondary' href='#'>Log In</a>
-            </li>
-            <li class='nav-item'>
-            <a  type='button' class='nav-link btn btn-dark' href='#'>Register</a>
-            </li>
-        </ul>
-    </div>
-    ";
-
-
-    if(isset($_SESSION["ShopperName"])) { 
-        //Display a greeting message, Change Password and logout links 
-        //after shopper has logged in.
-
-        $content1 = "
-        <div class='collapse navbar-collapse justify-content-end' id='collapsibleNavbar'>
-            <ul class='navbar-nav'>
-                <li class='nav-item'>
-                    <a type='button' class='nav-link btn btn-outline-secondary' href='#'>$_SESSION[NumCartItem]</a>
-                </li>
-                <li class='nav-item'>
-                    <a  type='button' class='nav-link btn btn-dark' href='#'>$_SESSION[ShopperName]</a>
-                    <ul class='navbar-nav'>
-                        <li class='nav-item'><a class='nav-link' href='#'>Change Password</a></li>
-                        <li class='nav-item'><a class='nav-link' href='#'>Update Profile</a></li>
-                        <li class='nav-item'><a class='nav-link' href='#'>Log Out</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        ";
-
-        // //Display number of item in cart
-        // if (isset($_SESSION["NumCartItem"]))
-        // {
-        //     if ($_SESSION["NumCartItem"] != 0)
-        //         $content1 .= ", $_SESSION[NumCartItem] item(s) in shopping cart";
-        // }
-    }
-
-    ?>
+    <?php echo $content1;?>
 </nav>
