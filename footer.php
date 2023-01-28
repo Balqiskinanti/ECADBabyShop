85f15ec3b52e552d7503a9e976d13fca2027b262
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2023-01-28 23:12:13 +0800

    Add footer

--- footer.php
+++ footer.php
@@ -4,17 +4,19 @@
 
 <hr/>
 
-<div id="footer" style="padding: 30px;justify-content: space-evenly; font-size: small;">
+<div id="footer" style="padding: 20px;justify-content: space-evenly; font-size: small;">
     <div class="container-fluid m-0">
         <div class="row">
             <div class="col" style="text-align: left;">
-                Logo
+                <img src='../ECADBabyShop/Images/Template/LongLogo.PNG' height='40'>
             </div>
             <div class="col" style="text-align: center;">
-                &#169 Copyright
+                &#169 Copyright ECADBabyShop
             </div>
             <div class="col" style="text-align: end;">
-                <a href="#" style="color: black;">Link 1</a> 
+                <a href="privacyPolicy.php" style="color: black;padding-right:10px">Privacy Policy</a>
+                <a href="faq.php" style="color: black;padding-right:10px">FAQ</a>  
+                <a href="mailto:ecadbabyshop@gmail.com" style="color: black;">Contact Us</a> 
             </div>
         </div>
     </div>
